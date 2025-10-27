17863ee86c2cb68a16b97cf6142290585b9b163d
Kevin Sims <simskevin267@gmail.com>
2025-10-26 20:50:11 -0500

    First draft

---
+++ TaskRecord.py
@@ -0,0 +1,12 @@
+class TaskRecord:
+    def __init__(self):
+        self.taskId
+        self.userId
+        self.title
+        self.description
+        self.dueDate
+        self.createDate
+        self.startDate
+        self.completeData
+
+        
