530a74832f260bb39d715cee7fa0da248b31ed51
Kevin Sims <simskevin267@gmail.com>
2025-11-03 00:09:33 -0600

    Project 1 commit

--- TaskRecord.py
+++ TaskRecord.py
@@ -3,75 +3,96 @@ import json
 
 class TaskRecord:
     def __init__(self):
-        self._taskId = int(0)
-        self._userId = int(0)
-        self._title = ""
-        self._description = ""
-        self._dueDate = datetime.now()
-        self._createDate = datetime.now()
-        self._startDate = datetime.now()
-        self._completeData = datetime.now()
+        self.record ={}
+        self.record["taskId"] = int(0)
+        self.record["userId"] = int(0)
+        self.record["title"] = ""
+        self.record["description"] = ""
+        self.record["dueDate"] = datetime.now()
+        self.record["createDate"] = datetime.now()
+        self.record["startDate"] = datetime.now()
+        self.record["completeData"] = datetime.now()
+        self.dateFormat = "%Y-%m-%d %H:%M:%S"
 
     @property
     def taskId(self):
-        return self._taskId
+        return self.record["taskId"]
     
     @taskId.setter
     def taskId(self,new_taskId):
-        self._taskId = new_taskId
+        self.record["taskId"] = new_taskId
 
     @property
     def userId(self):
-        return self._userId
+        return self.record["userId"]
     
     @userId.setter
     def userId(self,new_userId):
-        self._userId = new_userId
+        self.record["userId"] = new_userId
 
     @property
     def title(self):
-        return self._title
+        return self.record["title"]
     
     @title.setter
     def title(self,new_title):
-        self._title = new_title
+        self.record["title"] = new_title
 
     @property
     def description(self):
-        return self._description
+        return self.record["description"]
     
     @description.setter
     def description(self,new_desc):
-        self._description = new_desc
+        self.record["description"] = new_desc
 
     @property
     def dueDate(self):
-        return self._dueDate
+        return self.record["dueDate"]
     
     @dueDate.setter
     def dueDate(self,new_dueDate):
-        self._dueDate = new_dueDate
+         self.record["dueDate"] = new_dueDate
 
     @property
     def createDate(self):
-        return self._createDate
+        return self.record["createDate"]
     
     @createDate.setter
     def createDate(self,new_createDate):
-        self._createDate = new_createDate
+        self.record["createDate"] = new_createDate
 
     @property
     def startDate(self):
-        return self._startDate
+        return self.record["startDate"]
     
     @startDate.setter
     def startDate(self,new_startDate):
-        self._startDate = new_startDate
+        self.record["startDate"] = new_startDate
 
     @property
     def completeData(self):
-        return self._completeData
+        return self.record["completeData"]
     
     @createDate.setter
     def completeData(self,new_completeData):
-        self._completeData = new_completeData
+        self.record["completeData"] = new_completeData
+
+    def saveAsJson(self):
+        temp_record = {}
+        for key in self.record:
+            if type(self.record[key]) == datetime:
+                temp_record[key] = self.record[key].strftime(self.dateFormat)
+            else:
+                temp_record[key] = self.record[key]
+
+        return json.dumps(temp_record) 
+    
+    def loadJson(self,value):
+        local_record = json.loads(value)
+        for key in local_record:
+            if key in self.record:
+                if type(self.record[key]) == datetime:
+                    self.record[key] = datetime.strptime(local_record[key],self.dateFormat)
+            else:
+                self.record[key] = local_record[key]
---
+++ UserInfo.py
@@ -0,0 +1,71 @@
+import base64
+import json
+from datetime import datetime, timedelta
+
+class UserInfo:
+    def __init__(self):
+        self.info = {}
+        self.info["userId"] = ""
+        self.info["password"] = ""
+        self.info["lastLogin"] = None
+        self.info["failedLogin"] = None
+        self.info["failedCount"] = 0
+        self.dateFormat = "%Y-%m-%d %H:%M:%S"
+
+@property
+def userId(self):
+    return self.info["userId"]
+    
+@userId.setter
+def userId(self,new_userId):
+    self.info["userId"] = new_userId
+
+@property
+def password(self):
+    return self.info["password"]
+
+@password.setter
+def password(self,new_password):
+        self.info["password"] = new_password.encode('utf-8')
+
+def checkPassword(self, password):
+    return password.encode('utf-8') == self.info["password"]
+
+def setLoginTime(self,timeStr=None):
+    if timeStr == None:
+        self.info["lastLogin"] = datetime.now()
+    elif type(timeStr) == datetime:
+        self.info["lastLogin"] = timeStr
+    else:
+        self.info["lastLogin"] = datetime.strptime(timeStr,self.dateFormat)
+
+    self.info["failedCount"] = 0
+
+def setFailedLoginTime(self,timeStr=None):
+    if timeStr == None:
+        self.info["failedLogin"] = datetime.now()
+    elif type(timeStr) == datetime:
+        self.info["failedLogin"] = timeStr
+    else:
+        self.info["failedLogin"] = datetime.strptime(timeStr,self.dateFormat)
+
+    self.info["failedCount"] += 1
+
+def saveAsJson(self):
+        temp_record = {}
+        for key in self.info:
+            if type(self.info[key]) == datetime:
+                temp_record[key] = self.info[key].strftime(self.dateFormat)
+            else:
+                temp_record[key] = self.info[key]
+
+        return json.dumps(temp_record) 
+    
+def loadJson(self,value):
+    local_record = json.loads(value)
+    for key in local_record:
+        if key in self.info:
+            if type(self.info[key]) == datetime:
+                self.info[key] = datetime.strptime(local_record[key],self.dateFormat)
+        else:
+            self.info[key] = local_record[key]
